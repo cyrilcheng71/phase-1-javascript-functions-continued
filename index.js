97fa7b0a984c007d5ce7811e0494e67156431ba5
Man Cheng <cyrilcheng@protonmail.com>
2022-08-18 00:10:26 -0700

    commit tree dcde3d3b710dd17ddfcdf7b9e84b57cfd2134976

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // code your solution here
+function saturdayFun(activity = "roller-skate"){
+
+return `This Saturday, I want to ${activity}!`;
+
+}
+
+function mondayWork(activity= "go to the office"){
+    
+return `This Monday, I will ${activity}.`;
+
+}
+
+
+function wrapAdjective(flare ="*"){
+
+const innerFucntion = function(adj="special"){
+    return `You are ${flare}${adj}${flare}!`
+}
+return innerFucntion;
+
+
+}
